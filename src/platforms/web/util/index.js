0dc309ea40cb253520c7d546ee86740cc4f13047
liyuan <liyuan520310@163.com>
2019-12-29 16:38:48 +0800

    util/index.js

--- src/platforms/web/util/index.js
+++ src/platforms/web/util/index.js
@@ -1,10 +1,19 @@
-
+/*2019-12-29 16:38:29*/
 export * from './attrs.js'
 export * from './class.js'
 export * from './element.js'
 
-function query() {
-
+function query(el) {
+  if (typeof el === 'string') {
+    const selected = document.querySelector(el)
+    if (!selected) {
+      console.warn('Cannot find element: ' + el)
+      return document.createElement('div')
+    }
+    return selected
+  } else {
+    return el
+  }
 }
 
 export {
