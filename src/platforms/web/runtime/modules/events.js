c0d2398ca29ae5a4622889157228b2dc14c39f22
liyuan <liyuan520310@163.com>
2019-12-31 21:15:36 +0800

    init events

---
+++ src/platforms/web/runtime/modules/events.js
@@ -0,0 +1,4 @@
+
+import { isDef, isUnDef } from "../../../../core/util/index.js"
+import { updateListeners } from "../../../../core/vdom/helpers/index.js"
+import { withMacroTask, isIE, supportsPassive } from "../../../../core/util/index.js"
