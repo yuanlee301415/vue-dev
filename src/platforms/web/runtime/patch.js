bf5a8e7d54f5fdb6b7bbe137e05bfd4dd53d1c63
liyuan <liyuan520310@163.com>
2020-01-01 14:20:39 +0800

    runtime/patch

--- src/platforms/web/runtime/patch.js
+++ src/platforms/web/runtime/patch.js
@@ -1,4 +1,12 @@
+/*2020-1-1 14:19:56*/
 import * as nodeOps from './node-ops.js'
 import { createPatchFunction } from '../../../core/vdom/patch.js'
 import baseModules from '../../../core/vdom/modules/index.js'
 import platformModules from '../../web/runtime/modules/index.js'
+
+const modules = platformModules.concat(baseModules)
+const patch = createPatchFunction({ nodeOps, modules })
+
+export {
+  patch
+}
