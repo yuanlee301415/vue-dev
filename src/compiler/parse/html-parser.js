175d0c67659915a0ac23d78666a8b0dffcee9ce7
liyuan <liyuan520310@163.com>
2020-01-01 21:53:52 +0800

    Lock src/compiler/parse/html-parser

--- src/compiler/parse/html-parser.js
+++ src/compiler/parse/html-parser.js
@@ -1,7 +1,6 @@
-
+/*2020-1-1 21:53:3*/
 import { makeMap, no } from "../../shared/util.js"
 import { isNonPhrasingTag } from "../../platforms/web/compiler/util.js"
-import {canBeLeftOpenTag} from "../../platforms/web/compiler/util"
 
 const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/
 
@@ -45,6 +44,7 @@ function parseHTML(html, options) {
   const stack = []
   const expectHTML = options.expectHTML
   const isUnaryTag = options.isUnaryTag || no
+  const canBeLeftOpenTag = options.canBeLeftOpenTag || no
   let index = 0
   let last, lastTag
 
@@ -62,14 +62,104 @@ function parseHTML(html, options) {
               options.comment(html.substring(4, commentEnd))
             }
             advance(commentEnd + 3)
+            continue
           }
         }
 
+        if (conditionalComment.test(html)) {
+          const conditionalEnd = html.indexOf(']>')
+          if (conditionalEnd >= 0) {
+            advance(conditionalEnd + 2)
+            continue
+          }
+        }
+
+        // Doctype
+        const doctypeMatch = html.match(doctype)
+        if (doctypeMatch) {
+          advance(doctypeMatch[0].length)
+          continue
+        }
+
+        // End tag
+        const endTagMatch = html.match(endTag)
+        if (endTagMatch) {
+          const curIndex = index
+          advance(endTagMatch[0].length)
+          parseEndTag(endTagMatch[1], curIndex, index)
+          continue
+        }
+
+        // start tag
+        const startTagMatch = parseStartTag()
+        if (startTagMatch) {
+          handleStartTag(startTagMatch)
+          if (shouldIgnoreFirstNewline(lastTag, html)){
+            advance(1)
+          }
+          continue
+        }
       }
+
+      let text, rest, next
+      if (textEnd >= 0) {
+        rest = html.slice(textEnd)
+        while (
+          !endTag.test(rest) &&
+            !startTagOpen.test(rest) &&
+            !comment.test(rest) &&
+            !conditionalComment.test(rest)
+          ) {
+          next = rest.indexOf('<', 1)
+          if (next < 0) break
+          textEnd += next
+          rest = html.slice(textEnd)
+        }
+        text = html.substring(0, textEnd)
+        advance(textEnd)
+      }
+
+      if (textEnd < 0) {
+        text = html
+        html = ''
+      }
+
+      if (options.chars && text) {
+        options.chars(text)
+      }
+
+    } else {
+      let endTagLength = 0
+      const stackedTag = lastTag.toLowerCase()
+      const reStackedTag = reCache[stackedTag] || (reCache[stackedTag] = new RegExp('([\\s\\S]*?)(</' + stackedTag + '[^>]*>)', 'i'))
+      const rest = html.replace(reStackedTag, function (all, text, endTag) {
+        endTagLength = endTag.length
+        if (!isPlainTextElement(stackedTag) && stackedTag !== 'noscript') {
+          text = text.replace(/<!--([\s\S]*?)-->/g, '$1').replace(/<!\[CDATA\[([\s\S]*?)]]>/g, '$1')
+        }
+        if (shouldIgnoreFirstNewline(stackedTag, text)) {
+          text = text.slice(1)
+        }
+        if (options.chars) {
+          options.chars(text)
+        }
+        return ''
+      })
+      index += html.length - rest.length
+      html = rest
+      parseEndTag(stackedTag, index - endTagLength, index)
+    }
+
+    if (html === last) {
+      options.chars && options.chars(html)
+      if (!stack.length && options.warn) {
+        options.warn(`Mal-formatted tag at end of template: "${html}"`)
+      }
+      break
     }
   }
-  parseEndTag()
 
+  parseEndTag()
 
   function advance(n) {
     index += n
@@ -172,6 +262,9 @@ function parseHTML(html, options) {
       options.end && options.end(tagName, false, start, end)
     }
   }
-
 }
 
+export {
+  isPlainTextElement,
+  parseHTML
+}
