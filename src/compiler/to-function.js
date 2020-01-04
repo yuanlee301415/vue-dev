a48c4ca5d1bd089be92c558bd99920e4dc30b4bd
liyuan <liyuan520310@163.com>
2020-01-04 22:56:00 +0800

    override: compiler/to-function

--- src/compiler/to-function.js
+++ src/compiler/to-function.js
@@ -1,72 +1,102 @@
-/*2020-1-4 18:6:40*/
-import { noop, extend } from "../shared/util.js"
-import { warn as baseWarn, tip } from "../core/util/index.js"
+/*override*/
+/* @flow */
 
-function createFunction(code, errors) {
+import { noop, extend } from '../shared/util.js'
+import { warn as baseWarn, tip } from '../core/util/debug.js'
+
+/*
+type CompiledFunctionResult = {
+  render: Function;
+  staticRenderFns: Array<Function>;
+};
+*/
+
+function createFunction (code, errors) {
   try {
     return new Function(code)
   } catch (err) {
-    errors.push({ err, code})
+    errors.push({ err, code })
     return noop
   }
 }
 
-function createCompileToFunctionFn(compile) {
+export function createCompileToFunctionFn (compile) {
   const cache = Object.create(null)
 
-  return function compileToFunctions(template, options, vm) {
+  return function compileToFunctions (
+    template,
+    options,
+    vm
+  ) {
     options = extend({}, options)
     const warn = options.warn || baseWarn
     delete options.warn
 
-    try {
-      new Function(' return 1')
-    } catch (e) {
-      if (e.toString().match(/unsafe-eval|CSP/)) {
+    /* istanbul ignore if */
+    if ('process.env.NODE_ENV' !== 'production') {
+      // detect possible CSP restriction
+      try {
+        new Function('return 1')
+      } catch (e) {
+        if (e.toString().match(/unsafe-eval|CSP/)) {
+          warn(
+            'It seems you are using the standalone build of Vue.js in an ' +
+            'environment with Content Security Policy that prohibits unsafe-eval. ' +
+            'The template compiler cannot work in this environment. Consider ' +
+            'relaxing the policy to allow unsafe-eval or pre-compiling your ' +
+            'templates into render functions.'
+          )
+        }
+      }
+    }
+
+    // check cache
+    const key = options.delimiters
+      ? String(options.delimiters) + template
+      : template
+    if (cache[key]) {
+      return cache[key]
+    }
+
+    // compile
+    const compiled = compile(template, options)
+
+    // check compilation errors/tips
+    if ('process.env.NODE_ENV' !== 'production') {
+      if (compiled.errors && compiled.errors.length) {
         warn(
-          'It seems you are using the standalone build of Vue.js in an ' +
-          'environment with Content Security Policy that prohibits unsafe-eval. ' +
-          'The template compiler cannot work in this environment. Consider ' +
-          'relaxing the policy to allow unsafe-eval or pre-compiling your ' +
-          'templates into render functions.'
+          `Error compiling template:\n\n${template}\n\n` +
+          compiled.errors.map(e => `- ${e}`).join('\n') + '\n',
+          vm
+        )
+      }
+      if (compiled.tips && compiled.tips.length) {
+        compiled.tips.forEach(msg => tip(msg, vm))
+      }
+    }
+
+    // turn code into functions
+    const res = {}
+    const fnGenErrors = []
+    res.render = createFunction(compiled.render, fnGenErrors)
+    res.staticRenderFns = compiled.staticRenderFns.map(code => {
+      return createFunction(code, fnGenErrors)
+    })
+
+    // check function generation errors.
+    // this should only happen if there is a bug in the compiler itself.
+    // mostly for codegen development use
+    /* istanbul ignore if */
+    if ('process.env.NODE_ENV' !== 'production') {
+      if ((!compiled.errors || !compiled.errors.length) && fnGenErrors.length) {
+        warn(
+          `Failed to generate render function:\n\n` +
+          fnGenErrors.map(({ err, code }) => `${err.toString()} in\n\n${code}\n`).join('\n'),
+          vm
         )
       }
     }
 
-    const key = options.delimiters ? String(options.delimiters) + template : template
-    if (cache[key]) return cache[key]
-
-    const compiled = compile(template, options)
-    if (compiled.errors && compiled.errors.length) {
-      warn(
-        `Error compiling template:\n\n${template}\n\n` +
-        compiled.errors.map(e => `- ${e}`).join('\n') + '\n',
-        vm
-      )
-    }
-
-    if (compiled.tips && compiled.tips.length) {
-      compiled.tips.forEach(msg => tip(msg, vm))
-    }
-
-    const res = {}
-    const fnGenErrors = []
-    res.render = createFunction(compiled.render, fnGenErrors)
-    res.staticRenderFns = compiled.staticRenderFns.map(code => createFunction(code, fnGenErrors))
-
-    if ((!compiled.errors || !compiled.errors.length) && fnGenErrors.length) {
-      warn(
-        `Failed to generate render function:\n\n` +
-        fnGenErrors.map(({ err, code }) => `${err.toString()} in\n\n${code}\n`).join('\n'),
-        vm
-      )
-    }
-
-    return  res
+    return (cache[key] = res)
   }
-
-}
-
-export {
-  createCompileToFunctionFn
 }
