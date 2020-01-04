3809947340f54018baac314a8750c6d2ef7b64e6
liyuan <liyuan520310@163.com>
2020-01-04 18:27:40 +0800

    Lock src/compiler/index

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -1,10 +1,18 @@
+/*2020-1-4 18:27:29*/
 import { parse } from "./parse/index.js"
 import { optimize } from "./optimizer.js"
 import { generate } from "./codegen/index.js"
 import { createCompilerCreator } from './create-compiler.js'
 
-const createCompiler = createCompilerCreator(function baseCompile () {
-
+const createCompiler = createCompilerCreator(function baseCompile (template, options) {
+  const ast = parse(template.trim(), options)
+  optimize(ast, options)
+  const code = generate(ast, options)
+  return {
+    ast,
+    render: code.render,
+    staticRenderFns: code.staticRenderFns
+  }
 })
 
 export {
