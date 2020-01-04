a6357dd1650ce714e59ead126b58415191428a67
liyuan <liyuan520310@163.com>
2020-01-04 22:20:35 +0800

    override: directivies/model

--- src/compiler/directives/model.js
+++ src/compiler/directives/model.js
@@ -1,24 +1,43 @@
-/*2019-12-31 22:18:14*/
-function genComponentModel(el, value, modifiers) {
+/*override*/
+/* @flow */
+
+/**
+ * Cross-platform code generation for component v-model
+ */
+export function genComponentModel (
+  el,
+  value,
+  modifiers
+) {
   const { number, trim } = modifiers || {}
+
   const baseValueExpression = '$$v'
   let valueExpression = baseValueExpression
   if (trim) {
-    valueExpression = `(typeof ${baseValueExpression} === 'string' ? ${baseValueExpression}.trim() : ${baseValueExpression})`
+    valueExpression =
+      `(typeof ${baseValueExpression} === 'string'` +
+        `? ${baseValueExpression}.trim()` +
+        `: ${baseValueExpression})`
   }
   if (number) {
     valueExpression = `_n(${valueExpression})`
   }
-
   const assignment = genAssignmentCode(value, valueExpression)
+
   el.model = {
     value: `(${value})`,
     expression: `"${value}"`,
-    callback: `function (${baseValueExpression}){${assignment}}`
+    callback: `function (${baseValueExpression}) {${assignment}}`
   }
 }
 
-function genAssignmentCode(value, assignment) {
+/**
+ * Cross-platform codegen helper for generating v-model value assignment code.
+ */
+export function genAssignmentCode (
+  value,
+  assignment
+) {
   const res = parseModel(value)
   if (res.key === null) {
     return `${value}=${assignment}`
@@ -27,17 +46,39 @@ function genAssignmentCode(value, assignment) {
   }
 }
 
+/**
+ * Parse a v-model expression into a base path and a final key segment.
+ * Handles both dot-path and possible square brackets.
+ *
+ * Possible cases:
+ *
+ * - test
+ * - test[key]
+ * - test[test1[key]]
+ * - test["a"][key]
+ * - xxx.test[a[a].test1[key]]
+ * - test.xxx.a["asa"][test1[key]]
+ *
+ */
+
 let len, str, chr, index, expressionPos, expressionEndPos
 
-function parseModel(val) {
+/*
+type ModelParseResult = {
+  exp: string,
+  key: string | null
+}
+*/
+
+export function parseModel (val) {
   len = val.length
 
-  if (val.indexOf('[') < 0 || val.lastIndexOf(']') < len -1) {
+  if (val.indexOf('[') < 0 || val.lastIndexOf(']') < len - 1) {
     index = val.lastIndexOf('.')
     if (index > -1) {
       return {
         exp: val.slice(0, index),
-        key: `"${val.slice(index+1)}"`
+        key: '"' + val.slice(index + 1) + '"'
       }
     } else {
       return {
@@ -52,43 +93,43 @@ function parseModel(val) {
 
   while (!eof()) {
     chr = next()
+    /* istanbul ignore if */
     if (isStringStart(chr)) {
       parseString(chr)
-    } else if (chr === 0x5b) {
+    } else if (chr === 0x5B) {
       parseBracket(chr)
     }
   }
 
   return {
     exp: val.slice(0, expressionPos),
-    kye: val.slice(expressionPos + 1, expressionEndPos)
+    key: val.slice(expressionPos + 1, expressionEndPos)
   }
 }
 
-function next() {
+function next () {
   return str.charCodeAt(++index)
 }
 
-function eof() {
+function eof () {
   return index >= len
 }
 
-function isStringStart(chr) {
+function isStringStart (chr) {
   return chr === 0x22 || chr === 0x27
 }
 
-
-function parseBracket(chr) {
+function parseBracket (chr) {
   let inBracket = 1
-  expressionEndPos = index
+  expressionPos = index
   while (!eof()) {
     chr = next()
     if (isStringStart(chr)) {
       parseString(chr)
       continue
     }
-    if (chr === 0x5b) inBracket++
-    if (chr === 0x5d) inBracket--
+    if (chr === 0x5B) inBracket++
+    if (chr === 0x5D) inBracket--
     if (inBracket === 0) {
       expressionEndPos = index
       break
@@ -96,16 +137,12 @@ function parseBracket(chr) {
   }
 }
 
-function parseString(chr) {
+function parseString (chr) {
   const stringQuote = chr
   while (!eof()) {
     chr = next()
-    if (chr === stringQuote) break
+    if (chr === stringQuote) {
+      break
+    }
   }
-}
-
-export {
-  genComponentModel,
-  genAssignmentCode,
-  parseModel
 }
