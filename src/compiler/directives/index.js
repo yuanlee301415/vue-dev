c40be3071a885ec1984de0054a0c60e0639a2094
liyuan <liyuan520310@163.com>
2020-01-04 22:19:02 +0800

    override: directivies/index

--- src/compiler/directives/index.js
+++ src/compiler/directives/index.js
@@ -1,7 +1,9 @@
-/*2020-1-4 14:25:45*/
-import on from "./on.js"
-import bind from "./bind.js"
-import { noop } from "../../shared/util.js"
+/*override*/
+/* @flow */
+
+import on from './on.js'
+import bind from './bind.js'
+import { noop } from '../../shared/util.js'
 
 export default {
   on,
