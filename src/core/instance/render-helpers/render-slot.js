93bf889d6910ca9280631053cf996d15e10cb050
liyuan <liyuan520310@163.com>
2020-01-05 12:51:33 +0800

    override: instance/render-helpers/render-slot

--- src/core/instance/render-helpers/render-slot.js
+++ src/core/instance/render-helpers/render-slot.js
@@ -1,18 +1,41 @@
-import { extend } from '../../util/index.js'
+/*override*/
+/* @flow */
 
-function renderSlot(name, fallback, props, bindObject) {
+import { extend, warn, isObject } from '../../../core/util/index.js'
+
+/**
+ * Runtime helper for rendering <slot>
+ */
+export function renderSlot (
+  name,
+  fallback,
+  props,
+  bindObject
+) {
   const scopedSlotFn = this.$scopedSlots[name]
-  if (scopedSlotFn) {
+  if (scopedSlotFn) { // scoped slot
     props = props || {}
     if (bindObject) {
+      if ('process.env.NODE_ENV' !== 'production' && !isObject(bindObject)) {
+        warn(
+          'slot v-bind without argument expects an Object',
+          this
+        )
+      }
       props = extend(extend({}, bindObject), props)
     }
     return scopedSlotFn(props) || fallback
   } else {
-    return this.$slots[name] || fallback
+    const slotNodes = this.$slots[name]
+    // warn duplicate slot usage
+    if (slotNodes && 'process.env.NODE_ENV' !== 'production') {
+      slotNodes._rendered && warn(
+        `Duplicate presence of slot "${name}" found in the same render tree ` +
+        `- this will likely cause render errors.`,
+        this
+      )
+      slotNodes._rendered = true
+    }
+    return slotNodes || fallback
   }
-}
-
-export {
-  renderSlot
 }
