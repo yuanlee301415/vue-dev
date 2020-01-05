2c362ec811a5047f7a5aa89463e643e2eb200c3d
liyuan <liyuan520310@163.com>
2020-01-05 12:40:12 +0800

    override: global-api/index

--- src/core/global-api/index.js
+++ src/core/global-api/index.js
@@ -1,67 +1,63 @@
+/*override*/
+/* @flow */
+
 import config from '../config.js'
 import { initUse } from './use.js'
 import { initMixin } from './mixin.js'
 import { initExtend } from './extend.js'
 import { initAssetRegisters } from './assets.js'
 import { set, del } from '../observer/index.js'
-import { ASSET_TYPES} from "../../shared/constants.js"
+import { ASSET_TYPES } from '../../shared/constants.js'
 import builtInComponents from '../components/index.js'
-import { extend, nextTick, mergeOptions, defineReactive } from "../util/index.js"
 
-function initGlobalAPI(Vue) {
-  console.info('>>initGlobalAPI')
+import {
+  warn,
+  extend,
+  nextTick,
+  mergeOptions,
+  defineReactive
+} from '../util/index.js'
+
+export function initGlobalAPI (Vue) {
+  // config
   const configDef = {}
   configDef.get = () => config
+  if ('process.env.NODE_ENV' !== 'production') {
+    configDef.set = () => {
+      warn(
+        'Do not replace the Vue.config object, set individual fields instead.'
+      )
+    }
+  }
   Object.defineProperty(Vue, 'config', configDef)
-  console.log('Vue.config:', configDef)
 
+  // exposed util methods.
+  // NOTE: these are not considered part of the public API - avoid relying on
+  // them unless you are aware of the risk.
   Vue.util = {
+    warn,
     extend,
     mergeOptions,
     defineReactive
   }
-  console.log('Vue.util:', Vue.util)
 
   Vue.set = set
-  console.log('Vue.set')
-
   Vue.delete = del
-  console.log('Vue.delete')
-
   Vue.nextTick = nextTick
-  console.log('Vue.nextTick')
 
   Vue.options = Object.create(null)
-  console.log('Vue.options:', Vue.options)
-
-  ASSET_TYPES.forEach((type,i) => {
+  ASSET_TYPES.forEach(type => {
     Vue.options[type + 's'] = Object.create(null)
-    console.log(`Vue.options.${type}`)
   })
 
+  // this is used to identify the "base" constructor to extend all plain-object
+  // components with in Weex's multi-instance scenarios.
   Vue.options._base = Vue
-  console.log('Vue.options._base')
 
   extend(Vue.options.components, builtInComponents)
-  console.log('Vue.options.components:', Vue.options.components)
 
-  console.group('initUse')
   initUse(Vue)
-  console.groupEnd('initUse')
-
-  console.group('initUse')
   initMixin(Vue)
-  console.groupEnd('initUse')
-
-  console.group('initExtend')
   initExtend(Vue)
-  console.groupEnd('initExtend')
-
-  console.group('initAssetRegisters')
   initAssetRegisters(Vue)
-  console.groupEnd('initAssetRegisters')
-}
-
-export {
-  initGlobalAPI
 }
