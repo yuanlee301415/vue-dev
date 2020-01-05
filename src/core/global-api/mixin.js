413e2e2271bbab34bbc75712b71d14eadbec3742
liyuan <liyuan520310@163.com>
2020-01-05 12:41:08 +0800

    override: global-api/mixin

--- src/core/global-api/mixin.js
+++ src/core/global-api/mixin.js
@@ -1,14 +1,11 @@
-/*2019-12-22 20:51:8*/
+/*override*/
+/* @flow */
+
 import { mergeOptions } from '../util/index.js'
 
-function initMixin(Vue) {
+export function initMixin (Vue) {
   Vue.mixin = function (mixin) {
     this.options = mergeOptions(this.options, mixin)
     return this
   }
-  console.log('Vue.mixin')
-}
-
-export {
-  initMixin
 }
