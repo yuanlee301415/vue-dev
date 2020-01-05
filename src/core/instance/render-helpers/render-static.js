d6f90142cb16512686c9a3cd26372f6ef6794473
liyuan <liyuan520310@163.com>
2020-01-05 12:52:59 +0800

    override: instance/render-helpers/render-static

--- src/core/instance/render-helpers/render-static.js
+++ src/core/instance/render-helpers/render-static.js
@@ -1,12 +1,51 @@
-import { cloneVNodes, cloneVNode } from "../../vdom/vnode.js"
+/*override*/
+/* @flow */
 
-function markStaticNode(node, key, isOnce) {
-  node.isStatic = true
-  node.key = key
-  node.isOnce = isOnce
+import { cloneVNode, cloneVNodes } from '../../../core/vdom/vnode.js'
+
+/**
+ * Runtime helper for rendering static trees.
+ */
+export function renderStatic (
+  index,
+  isInFor
+) {
+  // static trees can be rendered once and cached on the contructor options
+  // so every instance shares the same cached trees
+  const renderFns = this.$options.staticRenderFns
+  const cached = renderFns.cached || (renderFns.cached = [])
+  let tree = cached[index]
+  // if has already-rendered static tree and not inside v-for,
+  // we can reuse the same tree by doing a shallow clone.
+  if (tree && !isInFor) {
+    return Array.isArray(tree)
+      ? cloneVNodes(tree)
+      : cloneVNode(tree)
+  }
+  // otherwise, render a fresh tree.
+  tree = cached[index] = renderFns[index].call(this._renderProxy, null, this)
+  markStatic(tree, `__static__${index}`, false)
+  return tree
 }
 
-function markStatic(tree, key, isOnce) {
+/**
+ * Runtime helper for v-once.
+ * Effectively it means marking the node as static with a unique key.
+ */
+export function markOnce (
+  tree,
+  index,
+  key
+) {
+  markStatic(tree, `__once__${index}${key ? `_${key}` : ``}`, true)
+  return tree
+}
+
+function markStatic (
+  tree,
+  key,
+  isOnce
+) {
   if (Array.isArray(tree)) {
     for (let i = 0; i < tree.length; i++) {
       if (tree[i] && typeof tree[i] !== 'string') {
@@ -18,21 +57,8 @@ function markStatic(tree, key, isOnce) {
   }
 }
 
-function renderStatic(index, isInFor) {
-  const renderFns = this.$options.staticRenderFns
-  const cached = renderFns.cached || (renderFns.cached = [])
-  let tree = cached[index]
-  if (tree && !isInFor) return Array.isArray(tree) ? cloneVNodes(tree) : cloneVNode(tree)
-  tree = cached[index] = renderFns[index].call(this._renderProxy, null, this)
-  markStatic(tree, `__static__${index}`, false)
-  return false
-}
-
-function markOnce(tree, index, key) {
-  markStatic(tree, `__once__${index}${key ? `_${key}` : ''}`, true)
-}
-
-export {
-  renderStatic,
-  markOnce
+function markStaticNode (node, key, isOnce) {
+  node.isStatic = true
+  node.key = key
+  node.isOnce = isOnce
 }
