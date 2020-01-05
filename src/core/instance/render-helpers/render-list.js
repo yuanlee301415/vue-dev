c6401473bff5c33d96040a0275e00deeaab0df89
liyuan <liyuan520310@163.com>
2020-01-05 12:50:26 +0800

    override: instance/render-helpers/render-list

--- src/core/instance/render-helpers/render-list.js
+++ src/core/instance/render-helpers/render-list.js
@@ -1,10 +1,19 @@
-import { isObject, isDef } from '../../util/index.js'
+/*override*/
+/* @flow */
 
-function renderList(val, render) {
+import { isObject, isDef } from '../../../core/util/index.js'
+
+/**
+ * Runtime helper for rendering v-for lists.
+ */
+export function renderList (
+  val,
+  render
+) {
   let ret, i, l, keys, key
   if (Array.isArray(val) || typeof val === 'string') {
     ret = new Array(val.length)
-    for (i = 0; l = val.length; i++) {
+    for (i = 0, l = val.length; i < l; i++) {
       ret[i] = render(val[i], i)
     }
   } else if (typeof val === 'number') {
@@ -16,15 +25,12 @@ function renderList(val, render) {
     keys = Object.keys(val)
     ret = new Array(keys.length)
     for (i = 0, l = keys.length; i < l; i++) {
+      key = keys[i]
       ret[i] = render(val[key], key, i)
     }
   }
   if (isDef(ret)) {
-    ret._isVList = true
+    (ret)._isVList = true
   }
   return ret
-}
-
-export {
-  renderList
 }
