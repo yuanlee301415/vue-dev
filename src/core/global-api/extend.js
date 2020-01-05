99c6fcbfbd727f540a2fb8ce99292ad320f516a3
liyuan <liyuan520310@163.com>
2020-01-05 12:38:46 +0800

    override: global-api/extend

--- src/core/global-api/extend.js
+++ src/core/global-api/extend.js
@@ -1,73 +1,102 @@
-/*2019-12-22 20:59:28*/
-import { ASSET_TYPES } from "../../shared/constants.js"
-import { extend, mergeOptions } from "../util/index.js"
+/*override*/
+/* @flow */
+
+import { ASSET_TYPES } from '../../shared/constants.js'
+import { warn, extend, mergeOptions } from '../util/index.js'
 import { defineComputed, proxy } from '../instance/state.js'
 
-function initExtend(Vue) {
-  console.log('>>initExtend')
-
+export function initExtend (Vue) {
+  /**
+   * Each instance constructor, including Vue, has a unique
+   * cid. This enables us to create wrapped "child
+   * constructors" for prototypal inheritance and cache them.
+   */
   Vue.cid = 0
   let cid = 1
 
-  Vue.extend = function (extendOptions = {}) {
+  /**
+   * Class inheritance
+   */
+  Vue.extend = function (extendOptions) {
+    extendOptions = extendOptions || {}
     const Super = this
     const SuperId = Super.cid
     const cachedCtors = extendOptions._Ctor || (extendOptions._Ctor = {})
-
-    if (cachedCtors[SuperId]) return cachedCtors[SuperId]
+    if (cachedCtors[SuperId]) {
+      return cachedCtors[SuperId]
+    }
 
     const name = extendOptions.name || Super.options.name
-    if (!/^[a-zA-Z][\w-]*$/.test(name)) {
-      console.warn(
-        'Invalid component name: "' + name + '". Component names ' +
-        'can only contain alphanumeric characters and the hyphen, ' +
-        'and must start with a letter.'
-      )
+    if ('process.env.NODE_ENV' !== 'production') {
+      if (!/^[a-zA-Z][\w-]*$/.test(name)) {
+        warn(
+          'Invalid component name: "' + name + '". Component names ' +
+          'can only contain alphanumeric characters and the hyphen, ' +
+          'and must start with a letter.'
+        )
+      }
     }
-    const Sub = function VueComponent(options) {
+
+    const Sub = function VueComponent (options) {
       this._init(options)
     }
     Sub.prototype = Object.create(Super.prototype)
     Sub.prototype.constructor = Sub
     Sub.cid = cid++
-    Sub.options = mergeOptions(Super.options, extendOptions)
-    Sub['super'] =Super
-    Sub.options.props && initProps(Sub)
-    Sub.options.computed && initComputed(Sub)
+    Sub.options = mergeOptions(
+      Super.options,
+      extendOptions
+    )
+    Sub['super'] = Super
+
+    // For props and computed properties, we define the proxy getters on
+    // the Vue instances at extension time, on the extended prototype. This
+    // avoids Object.defineProperty calls for each instance created.
+    if (Sub.options.props) {
+      initProps(Sub)
+    }
+    if (Sub.options.computed) {
+      initComputed(Sub)
+    }
+
+    // allow further extension/mixin/plugin usage
     Sub.extend = Super.extend
     Sub.mixin = Super.mixin
     Sub.use = Super.use
 
-    ASSET_TYPES.forEach(type => Sub[type] = Super[type])
-
+    // create asset registers, so extended classes
+    // can have their private assets too.
+    ASSET_TYPES.forEach(function (type) {
+      Sub[type] = Super[type]
+    })
+    // enable recursive self-lookup
     if (name) {
       Sub.options.components[name] = Sub
     }
 
+    // keep a reference to the super options at extension time.
+    // later at instantiation we can check if Super's options have
+    // been updated.
     Sub.superOptions = Super.options
     Sub.extendOptions = extendOptions
     Sub.sealedOptions = extend({}, Sub.options)
 
+    // cache constructor
     cachedCtors[SuperId] = Sub
     return Sub
   }
-  console.log(`Vue.extend`)
 }
 
-function initProps(Comp) {
+function initProps (Comp) {
   const props = Comp.options.props
   for (const key in props) {
-    proxy(Comp.prototype, '__props__', key)
+    proxy(Comp.prototype, `_props`, key)
   }
 }
 
-function initComputed(Comp) {
+function initComputed (Comp) {
   const computed = Comp.options.computed
   for (const key in computed) {
-    defineComputed(Comp.prototype, key, computed([key]))
+    defineComputed(Comp.prototype, key, computed[key])
   }
-}
-
-export {
-  initExtend
 }
